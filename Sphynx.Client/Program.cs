3654a8b03646554556c5661e00fc07470e74b9af
Ark1409 <ark1409dev@gmail.com>
2023-10-15 23:59:41 -0700

    Added client project and Hello World

---
+++ Sphynx.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sphynx.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
