a6c4d93717351e5c81b0ada8a5da464e1ffe9a9a
Specyy <specyydev@gmail.com>
2023-10-16 00:40:44 -0700

    Create server project

---
+++ Sphynx.Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sphynx.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
