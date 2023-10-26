a11dfbc814385b157b76de3a85352d198a6155d7
Specyy <specyydev@gmail.com>
2023-10-25 21:34:24 -0700

    Added default states for app instance

---
+++ Sphynx.Server/State/ISphynxState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphynx.Server.State
+{
+    /// <summary>
+    /// Represents an applicaiton state.
+    /// </summary>
+    public interface ISphynxState
+    {
+        /// <summary>
+        /// Runs the currents application state, blocking until it is finished; and returns the next state, or null if there are no
+        /// more states to run.
+        /// </summary>
+        /// <returns>The next state to run, or null if there are no more.</returns>
+        ISphynxState? Run();
+    }
+}
---
+++ Sphynx.Server/State/MenuState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphynx.Server.State
+{
+    public sealed class MenuState : ISphynxState
+    {
+        public ISphynxState? Run()
+        {
+            Console.WriteLine("#######################################################");
+            Console.WriteLine("##                   SPHYNX SERVER                   ##");
+            Console.WriteLine("#######################################################");
+            Console.WriteLine();
+
+            return null;
+        }
+    }
+}
