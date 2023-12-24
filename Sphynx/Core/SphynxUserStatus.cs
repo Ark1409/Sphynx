5ad2518d2160b76dd523048baade5f3aef0e1696
Specyy <specyydev@gmail.com>
2023-12-24 00:53:33 -0800

    Finalized Version struct and separated user status from user info/session

---
+++ Sphynx/Core/SphynxUserStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphynx.Core
+{
+    /// <summary>
+    /// Possible statuses for a <see cref="SphynxSessionUser"/>.
+    /// </summary>
+    public enum SphynxUserStatus : byte
+    {
+        OFFLINE = 0,
+        ONLINE,
+        AWAY,
+        DND
+    }
+}
