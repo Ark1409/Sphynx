a4880785abe5a6c97636f4cae51e482237f66565
Specyy <specyydev@gmail.com>
2023-12-30 02:24:47 -0800

    Create chat inivite packet

---
+++ Sphynx/Packet/Request/ChatInviteRequestPacket.cs
@@ -0,0 +1,65 @@
+﻿using Sphynx.Packet.Response;
+
+namespace Sphynx.Packet.Request
+{
+    /// <inheritdoc cref="SphynxPacketType.CHAT_INV_REQ"/>
+    public sealed class ChatInviteRequestPacket : SphynxRequestPacket, IEquatable<ChatInviteRequestPacket>
+    {
+        /// <summary>
+        /// ID of user to add to the chat room.
+        /// </summary>
+        public Guid UserId { get; set; }
+        
+        /// <summary>
+        /// Room ID assigned to the newly created room.
+        /// </summary>
+        public Guid RoomId { get; set; }
+
+        /// <inheritdoc/>
+        public override SphynxPacketType PacketType => SphynxPacketType.CHAT_CREATE_RES;
+
+        private const int ROOM_ID_OFFSET = 0;
+        private const int ROOM_ID_SIZE = 16;
+
+        /// <summary>
+        /// Creates a <see cref="ChatInviteRequestPacket"/>.
+        /// </summary>
+        /// <param name="contents">Packet contents, excluding the header.</param>
+        public ChatInviteRequestPacket(ReadOnlySpan<byte> contents)
+        {
+            RoomId = new Guid(contents.Slice(ROOM_ID_OFFSET, ROOM_ID_SIZE));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ChatCreateResponsePacket"/>.
+        /// </summary>
+        /// <param name="roomId">Room ID assigned to the newly created room.</param>
+        public ChatInviteRequestPacket(Guid roomId)
+        {
+            RoomId = roomId;
+        }
+
+        /// <inheritdoc/>
+        public override byte[] Serialize()
+        {
+            int contentSize = ROOM_ID_SIZE;
+
+            byte[] serializedBytes = new byte[SphynxResponseHeader.HEADER_SIZE + contentSize];
+            var serialzationSpan = new Span<byte>(serializedBytes);
+
+            SerializeHeader(serialzationSpan.Slice(0, SphynxResponseHeader.HEADER_SIZE), contentSize);
+            SerializeContents(serialzationSpan.Slice(SphynxResponseHeader.HEADER_SIZE));
+
+            return serializedBytes;
+        }
+
+        private void SerializeContents(Span<byte> buffer)
+        {
+            // TODO: Handle writing error
+            RoomId.TryWriteBytes(buffer.Slice(ROOM_ID_OFFSET, ROOM_ID_SIZE));
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(ChatInviteRequestPacket? other) => UserId == other?.UserId && RoomId == other?.RoomId;
+    }
+}
---
+++ Sphynx/Packet/Response/ChatInviteResponsePacket.cs
@@ -0,0 +1,74 @@
+﻿using Sphynx.Utils;
+
+namespace Sphynx.Packet.Response
+{
+    /// <inheritdoc cref="SphynxPacketType.CHAT_INV_RES"/>
+    public sealed class ChatInviteResponsePacket : SphynxResponsePacket
+    {
+        /// <summary>
+        /// Error code chat invitation.
+        /// </summary>
+        public SphynxErrorCode ErrorCode { get; set; }
+
+        /// <summary>
+        /// The message for the <see cref="ErrorCode"/>.
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+        /// <inheritdoc/>
+        public override SphynxPacketType PacketType => SphynxPacketType.LOGIN_RES;
+
+        private const int ERROR_CODE_OFFSET = 0;
+        private const int ERROR_MSG_SIZE_OFFSET = ERROR_CODE_OFFSET + sizeof(SphynxErrorCode);
+        private const int ERROR_MSG_OFFSET = ERROR_MSG_SIZE_OFFSET + sizeof(int);
+
+        /// <summary>
+        /// Creates a <see cref="ChatInviteResponsePacket"/>.
+        /// </summary>
+        /// <param name="contents">Packet contents, excluding the header.</param>
+        public ChatInviteResponsePacket(ReadOnlySpan<byte> contents)
+        {
+            ErrorCode = (SphynxErrorCode)contents[ERROR_CODE_OFFSET];
+
+            int errorMsgSize = contents.Slice(ERROR_MSG_OFFSET, sizeof(int)).ReadInt32();
+            ErrorMessage = TEXT_ENCODING.GetString(contents.Slice(ERROR_MSG_OFFSET, errorMsgSize));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ChatDeleteResponsePacket"/>.
+        /// </summary>
+        /// <param name="errorCode">Error code for chat invitation..</param>
+        /// <param name="errorMessage">Error message for <paramref name="errorCode"/>.</param>
+        public ChatInviteResponsePacket(SphynxErrorCode errorCode, string errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+
+        /// <inheritdoc/>
+        public override byte[] Serialize()
+        {
+            int errorMsgSize = TEXT_ENCODING.GetByteCount(ErrorMessage);
+            int contentSize = sizeof(SphynxErrorCode) + sizeof(int) + errorMsgSize;
+
+            byte[] serializedBytes = new byte[SphynxResponseHeader.HEADER_SIZE + contentSize];
+            var serialzationSpan = new Span<byte>(serializedBytes);
+
+            SerializeHeader(serialzationSpan.Slice(0, SphynxResponseHeader.HEADER_SIZE), contentSize);
+            SerializeContents(serialzationSpan.Slice(SphynxResponseHeader.HEADER_SIZE), errorMsgSize);
+
+            return serializedBytes;
+        }
+
+        private void SerializeContents(Span<byte> buffer, int errorMsgSize)
+        {
+            buffer[ERROR_CODE_OFFSET] = (byte)ErrorCode;
+
+            errorMsgSize.WriteBytes(buffer.Slice(ERROR_MSG_OFFSET, sizeof(int)));
+            TEXT_ENCODING.GetBytes(ErrorMessage, buffer.Slice(ERROR_MSG_OFFSET, errorMsgSize));
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(ChatDeleteResponsePacket? other) => ErrorCode == other?.ErrorCode && ErrorMessage == other?.ErrorMessage;
+    }
+}
