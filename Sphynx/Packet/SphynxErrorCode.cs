77bc3424172635091dd310079ab043bda44415ab
Specyy <specyydev@gmail.com>
2023-12-31 17:27:39 -0800

    Added more error codes

---
+++ Sphynx/Packet/SphynxErrorCode.cs
@@ -0,0 +1,38 @@
+﻿namespace Sphynx.Packet
+{
+    /// <summary>
+    /// Error code for packet responses.
+    /// </summary>
+    public enum SphynxErrorCode : byte
+    {
+        /// <summary>
+        /// Action has been completed succesfully.
+        /// </summary>
+        SUCCESS = 0,
+
+        /// <summary>
+        /// Invalid (unknown) email address at login.
+        /// </summary>
+        INVALID_EMAIL,
+
+        /// <summary>
+        /// Invalid password for authentication (whether it be account email at login or room password).
+        /// </summary>
+        INVALID_PASSWORD,
+
+        /// <summary>
+        /// Invalid session ID for user when performing an action.
+        /// </summary>
+        INVALID_SESSION,
+
+        /// <summary>
+        /// When the user attempts to complete an action but does not have sufficient permissions.
+        /// </summary>
+        INSUFFICIENT_PERMS,
+
+        /// <summary>
+        /// If an error occurs during packet initialization.
+        /// </summary>
+        FAILED_INIT,
+    }
+}
