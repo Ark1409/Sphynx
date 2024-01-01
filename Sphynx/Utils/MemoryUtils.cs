8f597ad8b794270ddd16f9f7936f1eeae64e4808
Specyy <specyydev@gmail.com>
2023-12-31 22:38:19 -0800

    Made util methods all internal

--- Sphynx/Utils/MemoryUtils.cs
+++ Sphynx/Utils/MemoryUtils.cs
@@ -2,7 +2,7 @@
 {
     internal static class MemoryUtils
     {
-        public static void WriteBytes(this int src, Span<byte> buffer, int offset = 0)
+        internal static void WriteBytes(this int src, Span<byte> buffer, int offset = 0)
         {
             buffer[offset] = (byte)(src & 0xFF);
             buffer[offset + 1] = (byte)((src >> 8) & 0xFF);
@@ -10,7 +10,7 @@
             buffer[offset + 3] = (byte)((src >> 24) & 0xFF);
         }
 
-        public static void WriteBytes(this uint src, Span<byte> buffer, int offset = 0)
+        internal static void WriteBytes(this uint src, Span<byte> buffer, int offset = 0)
         {
             buffer[offset] = (byte)(src & 0xFF);
             buffer[offset + 1] = (byte)((src >> 8) & 0xFF);
@@ -18,13 +18,13 @@
             buffer[offset + 3] = (byte)((src >> 24) & 0xFF);
         }
 
-        public static void WriteBytes(this ushort src, Span<byte> buffer, int offset = 0)
+        internal static void WriteBytes(this ushort src, Span<byte> buffer, int offset = 0)
         {
             buffer[offset] = (byte)(src & 0xFF);
             buffer[offset + 1] = (byte)((src >> 8) & 0xFF);
         }
 
-        public static int ReadInt32(this ReadOnlySpan<byte> buffer, int offset = 0)
+        internal static int ReadInt32(this ReadOnlySpan<byte> buffer, int offset = 0)
         {
             int number = buffer[offset + 3] << 24;
             number |= buffer[offset + 2] << 16;
@@ -33,7 +33,7 @@
             return number;
         }
 
-        public static uint ReadUInt32(this ReadOnlySpan<byte> buffer, int offset = 0)
+        internal static uint ReadUInt32(this ReadOnlySpan<byte> buffer, int offset = 0)
         {
             uint number = (uint)(buffer[offset + 3] << 24);
             number |= (uint)(buffer[offset + 2] << 16);
@@ -42,7 +42,7 @@
             return number;
         }
 
-        public static ushort ReadUInt16(this ReadOnlySpan<byte> buffer, int offset = 0)
+        internal static ushort ReadUInt16(this ReadOnlySpan<byte> buffer, int offset = 0)
         {
             ushort number = (ushort)(buffer[offset + 1] << 8);
             number |= buffer[offset];
