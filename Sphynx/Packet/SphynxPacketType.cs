bc9a5516868fc8a21981994429cd32a4f2ed31e1
Specyy <specyydev@gmail.com>
2024-01-02 01:46:02 -0800

    Made new, more sensible packet types

--- Sphynx/Packet/SphynxPacketType.cs
+++ Sphynx/Packet/SphynxPacketType.cs
@@ -8,83 +8,145 @@
         /// <summary>
         /// Safe-guard against corrupted or uninitialized data. Server simply discards packet contents.
         /// </summary>
-        NOP = 0x00000000,
+        NOP = 0x00000000u,
 
         /// <summary>
-        /// A login request packet sent from the client to the server with the necessary authentication information.
+        /// A login request packet sent from client to server with necessary authentication information.
         /// </summary>
-        LOGIN_REQ = 0x00000001,
+        LOGIN_REQ = 0x00000001u,
 
         /// <summary>
-        /// A login response packet sent from the server to the client with authentication status information.
+        /// A response sent from server to client to a <see cref="LOGIN_REQ"/> packet indicating whether the login
+        /// was successful.
         /// </summary>
-        LOGIN_RES = 0x80000001,
+        LOGIN_RES = 0x01000001u,
 
         /// <summary>
-        /// A packet that is sent from the client to the server when a user enters a message in a chat room. 
+        /// A packet sent from client to server when a user enters a message in a chat room. 
         /// </summary>
-        MSG_REQ = 0x00000002,
+        MSG_REQ = 0x00000002u,
 
         /// <summary>
-        /// Broadcast packet sent from server to all other clients which includes the message that was contained within 
+        /// A response sent from server to client to a <see cref="LOGIN_REQ"/> packet.
+        /// </summary>
+        MSG_RES = 0x01000002u,
+
+        /// <summary>
+        /// A broadcast packet sent from server to all other recipients containing the message that sent from 
         /// a <see cref="MSG_REQ"/> packet.
         /// </summary>
-        MSG_RES = 0x80000002,
+        MSG_BCAST = 0x02000001u,
 
         /// <summary>
-        /// Chat creation request packet sent from client to server including necessary chat creation information.
+        /// A chat creation request packet sent from client to server including necessary chat creation information.
         /// </summary>
-        CHAT_CREATE_REQ = 0x00000004,
+        CHAT_CREATE_REQ = 0x00000004u,
 
         /// <summary>
-        /// Chat creation response packet sent from server to client which returns room chat information.
+        /// A chat creation response packet sent from server to client which returns room chat information.
         /// </summary>
-        CHAT_CREATE_RES = 0x80000004,
+        CHAT_CREATE_RES = 0x01000004u,
 
         /// <summary>
-        /// A packet that is sent from the client to the server when a user decides to add another user to a chat room.
+        /// A packet sent from client to server when a user joins a chat room.
         /// </summary>
-        CHAT_INV_REQ = 0x00000008,
+        CHAT_JOIN_REQ = 0x00000005u,
 
         /// <summary>
-        /// A packet sent from server to requesting client indicating whether or not the invitation was successful.
+        /// A packet sent from server to requesting client indicating whether or not the joining was successful.
         /// </summary>
-        // TOOD: Perhaps later allow users to choose whether they can want to join the chat room after they've been added
-        CHAT_INV_RES = 0x80000008,
+        CHAT_JOIN_RES = 0x01000005u,
 
         /// <summary>
-        /// A packet that is sent from the client to server when a user attempts to kick another from a chat room.
+        /// A broadcast packet sent from server to all other clients within the chat room indicating that a user
+        /// has joined.
         /// </summary>
-        CHAT_KICK_REQ = 0x00000010,
+        CHAT_JOIN_BCAST = 0x02000002u,
+
+        /// <summary>
+        /// A packet sent from client to server when a leaves joins a chat room.
+        /// </summary>
+        CHAT_LEAVE_REQ = 0x01000006u,
+
+        /// <summary>
+        /// A packet sent from server to requesting client indicating whether or not the leaving was successful.
+        /// </summary>
+        CHAT_LEAVE_RES = 0x01000006u,
+
+        /// <summary>
+        /// A broadcast packet sent from server to all other clients within the chat room indicating that a user
+        /// has left.
+        /// </summary>
+        CHAT_LEAVE_BCAST = 0x02000003u,
+
+        /// <summary>
+        /// A packet sent from client to server when a user attempts to kick another from a chat room.
+        /// </summary>
+        CHAT_KICK_REQ = 0x00000007u,
 
         /// <summary>
         /// A confirmation packet sent from server to client indicating whether the user could be kicked.
         /// </summary>
-        CHAT_KICK_RES = 0x80000010,
+        CHAT_KICK_RES = 0x01000007u,
+
+        /// <summary>
+        /// A broadcast packet sent from server to all other clients in the chat room that a user has been kicked.
+        /// </summary>
+        CHAT_KICK_BCAST = 0x02000004u,
 
         /// <summary>
         /// A chat room deletion request packet sent from client to server.
         /// </summary>
-        CHAT_DEL_REQ = 0x00000020,
+        CHAT_DEL_REQ = 0x00000008u,
 
         /// <summary>
         /// A confirmation packet sent from server to client indicating whether the chat room could be deleted.
         /// </summary>
-        CHAT_DEL_RES = 0x80000020,
+        CHAT_DEL_RES = 0x01000008u,
+
+        /// <summary>
+        /// A broadcast packet sent from server to all other clients in the chat room indicating that it has been deleted.
+        /// </summary>
+        CHAT_DEL_BCAST = 0x02000005u,
 
         /// <summary>
         /// A packet that is sent from the client to the server when a user selects to enter a specific chat room on their screen.
         /// </summary>
-        CHAT_SELECT_REQ = 0x00000040,
+        CHAT_SELECT_REQ = 0x00000009u,
 
         /// <summary>
         /// A packet sent from server to client containing all information required when loading a chat room.
         /// </summary>
-        CHAT_SELECT_RES = 0x80000040,
+        CHAT_SELECT_RES = 0x01000009u,
 
         /// <summary>
-        /// A packet that is sent from the client to the server when a user attempts to logout.
+        /// A packet that is sent from client to server when a user attempts to logout.
         /// </summary>
-        LOGOUT_REQ = 0x00000080,
+        LOGOUT_REQ = 0x0000000Au,
+
+        /// <summary>
+        /// A confirmation packet that is sent from sever to client indicating whether the logout was successful.
+        /// </summary>
+        LOGOUT_RES = 0x0100000Au,
+
+        /// <summary>
+        /// A packet that is sent from the client to resolve room information.
+        /// </summary>
+        ROOM_INFO_REQ = 0x0000000Bu,
+
+        /// <summary>
+        /// A packet that is sent from server to the client containing resolved room information.
+        /// </summary>
+        ROOM_INFO_RES = 0x0100000Bu,
+
+        /// <summary>
+        /// A packet that is sent from the client to resolve user information.
+        /// </summary>
+        USER_INFO_REQ = 0x0000000Cu,
+
+        /// <summary>
+        /// A packet that is sent from server to the client containing resolved user information.
+        /// </summary>
+        USER_INFO_RES = 0x0100000Cu,
     }
 }
