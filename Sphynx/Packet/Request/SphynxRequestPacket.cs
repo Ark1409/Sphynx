894bea4606e93ac4e0085c02715fbf3f59dd5f5a
Specyy <specyydev@gmail.com>
2024-01-02 04:30:33 -0800

    Loosened packet hierarchy - allowed packets to be more than just request and response

---
+++ Sphynx/Packet/Request/SphynxRequestPacket.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Sphynx.Packet.Request
+{
+    /// <summary>
+    /// Represents a request packet.
+    /// </summary>
+    public abstract class SphynxRequestPacket : SphynxPacket
+    {
+        /// <summary>
+        /// The user ID of the requesting user.
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// The session ID for the requesting user.
+        /// </summary>s
+        public Guid SessionId { get; set; }
+
+        protected const int USER_ID_OFFSET = 0;
+        protected const int SESSION_ID_OFFSET = USER_ID_OFFSET + GUID_SIZE;
+        protected const int DEFAULT_CONTENT_SIZE = SESSION_ID_OFFSET + GUID_SIZE;
+
+        /// <summary>
+        /// Creates a new <see cref="SphynxRequestPacket"/>.
+        /// </summary>
+        public SphynxRequestPacket() : this(Guid.Empty, Guid.Empty)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SphynxRequestPacket"/>.
+        /// </summary>
+        /// <param name="userId">The user ID of the requesting user.</param>
+        /// <param name="sessionId">The session ID for the requesting user.</param>
+        public SphynxRequestPacket(Guid userId, Guid sessionId)
+        {
+            UserId = userId;
+            SessionId = sessionId;
+        }
+
+        /// <summary>
+        /// Attempts to retrieve a <see cref="UserId"/> and <see cref="SessionId"/> from the raw <paramref name="buffer"/> bytes.
+        /// </summary>
+        /// <param name="buffer">The raw bytes to deserialize the data from.</param>
+        /// <param name="userId">The deserialized user ID.</param>
+        /// <param name="sessionId">The deserialized session ID.</param>
+        /// <returns>true if the data could be deserialized; false otherwise.</returns>
+        protected static bool TryDeserialize(ReadOnlySpan<byte> buffer, [NotNullWhen(true)] out Guid? userId, [NotNullWhen(true)] out Guid? sessionId)
+        {
+            if (buffer.Length >= GUID_SIZE * 2)
+            {
+                userId = new Guid(buffer.Slice(USER_ID_OFFSET, GUID_SIZE));
+                sessionId = new Guid(buffer.Slice(SESSION_ID_OFFSET, GUID_SIZE));
+                return true;
+            }
+
+            userId = null;
+            sessionId = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Attempts to serialize this <see cref="SphynxRequestPacket"/> into the <paramref name="buffer"/>.
+        /// </summary>
+        /// <param name="buffer">The buffer to serialize this packet into.</param>
+        /// <returns>true if this packet could be serialized; false otherwise.</returns>
+        protected bool TrySerialize(Span<byte> buffer)
+        {
+            if (buffer.Length >= GUID_SIZE * 2)
+            {
+                UserId.TryWriteBytes(buffer[..GUID_SIZE]);
+                SessionId.TryWriteBytes(buffer.Slice(SESSION_ID_OFFSET, GUID_SIZE));
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Indicates whether the current packet has the same user and session ID as another request packet.
+        /// </summary>
+        /// <param name="other">A request packet to compare with this request packet.</param>
+        /// <returns>true if the current packet is equal to the other parameter; otherwise, false.</returns>
+        protected bool Equals(SphynxRequestPacket? other) => base.Equals(other) && UserId == other?.UserId && SessionId == other?.SessionId;
+    }
+}
--- Sphynx/Packet/SphynxPacketHeader.cs
+++ Sphynx/Packet/SphynxPacketHeader.cs
@@ -1,14 +1,21 @@
-﻿namespace Sphynx.Packet
+﻿using Sphynx.Utils;
+
+namespace Sphynx.Packet
 {
     /// <summary>
     /// Represents the header of a <see cref="SphynxPacket"/>.
     /// </summary>
-    public abstract class SphynxPacketHeader : IEquatable<SphynxPacketHeader>
+    public sealed class SphynxPacketHeader : IEquatable<SphynxPacketHeader>
     {
         /// <summary>
         /// The packet signature to safe-guards against corrupted packets.
         /// </summary>
         public const ushort SIGNATURE = 0x5350;
+
+        /// <summary>
+        /// The size of this particular header in bytes.
+        /// </summary>
+        public const short HEADER_SIZE = 10;
 
         /// <summary>
         /// The type of this packet.
@@ -20,10 +27,9 @@
         /// </summary>
         public int ContentSize { get; set; }
 
-        /// <summary>
-        /// Returns the size of this particular header in bytes.
-        /// </summary>
-        public abstract int HeaderSize { get; }
+        private const int SIGNATURE_OFFSET = 0;
+        private const int PACKET_TYPE_OFFSET = SIGNATURE_OFFSET + sizeof(ushort);
+        private const int CONTENT_SIZE_OFFSET = PACKET_TYPE_OFFSET + sizeof(SphynxPacketType);
 
         /// <summary>
         /// Creates a new <see cref="SphynxPacketHeader"/>.
@@ -37,39 +43,54 @@
         }
 
         /// <summary>
-        /// Checks whether the <paramref name="packetType"/> is a <see cref="Request.SphynxRequestHeader"/>.
+        /// Creates a new <see cref="SphynxPacketHeader"/> from the <paramref name="packetHeader"/>.
         /// </summary>
-        /// <param name="packetType"></param>
-        /// <returns>true if the <paramref name="packetType"/> is meant for a <see cref="Request.SphynxRequestHeader"/>;
-        /// false otherwise.</returns>
-        public static bool IsRequest(SphynxPacketType packetType) => (int)packetType > 0;
+        /// <param name="packetHeader">The raw bytes for a <see cref="SphynxPacketHeader"/>.</param>
+        /// <param name="header">The deserialized header.</param>
+        public static bool TryDeserialize(ReadOnlySpan<byte> packetHeader, out SphynxPacketHeader? header)
+        {
+            if (SIGNATURE == packetHeader.ReadUInt16(SIGNATURE_OFFSET) && packetHeader.Length >= HEADER_SIZE)
+            {
+                var packetType = (SphynxPacketType)packetHeader.ReadUInt32(PACKET_TYPE_OFFSET);
+                int contentSize = packetHeader.ReadInt32(CONTENT_SIZE_OFFSET);
 
-        /// <summary>
-        /// Checks whether the <paramref name="packetType"/> is a <see cref="Response.SphynxResponseHeader"/>.
-        /// </summary>
-        /// <param name="packetType"></param>
-        /// <returns>true if the <paramref name="packetType"/> is meant for a <see cref="Response.SphynxResponseHeader"/>;
-        /// false otherwise.</returns>
-        public static bool IsResponse(SphynxPacketType packetType) => (int)packetType < 0;
+                header = new SphynxPacketHeader(packetType, contentSize);
+                return true;
+            }
+
+            header = null;
+            return false;
+        }
 
         /// <summary>
         /// Serializes this packet header into a tightly-packed byte array.
         /// </summary>
         /// <returns>This packet header serialized as a byte array.</returns>
-        public virtual byte[] Serialize()
+        public byte[] Serialize()
         {
-            byte[] bytes = new byte[HeaderSize];
-            Serialize(bytes);
+            byte[] bytes = new byte[HEADER_SIZE];
+            TrySerialize(bytes);
             return bytes;
         }
 
         /// <summary>
-        /// Serializes this header into a buffer of bytes.
+        /// Attempts to serializes this header into a buffer of bytes.
         /// </summary>
         /// <param name="buffer">The buffer to serialize this header into.</param>
-        public abstract void Serialize(Span<byte> buffer);
+        public bool TrySerialize(Span<byte> buffer)
+        {
+            if (buffer.Length >= HEADER_SIZE)
+            {
+                SIGNATURE.WriteBytes(buffer, SIGNATURE_OFFSET);
+                ((uint)PacketType).WriteBytes(buffer, PACKET_TYPE_OFFSET);
+                ContentSize.WriteBytes(buffer, CONTENT_SIZE_OFFSET);
+                return true;
+            }
+
+            return false;
+        }
 
         /// <inheritdoc/>
-        public virtual bool Equals(SphynxPacketHeader? other) => PacketType == other?.PacketType && ContentSize == other?.ContentSize;
+        public bool Equals(SphynxPacketHeader? other) => PacketType == other?.PacketType && ContentSize == other?.ContentSize;
     }
 }
