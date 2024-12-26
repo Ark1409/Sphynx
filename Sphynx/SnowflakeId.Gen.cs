8f87a46392751974edf7b3ce42f727351fad70d3
Specyy <121744367+Specyy@users.noreply.github.com>
2024-12-26 05:19:08 -0800

    Finished SnowflakeId generation

--- Sphynx/SnowflakeId.Gen.cs
+++ Sphynx/SnowflakeId.Gen.cs
@@ -1,127 +1,106 @@
 // Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
+using System.Net;
 
 namespace Sphynx
 {
+    //
+    // Generation code for a SnowflakeId.
+    //
+    // The current implementation was profiled against many (functional) lockless alternatives,
+    // and while most performed about the same or even better than, this one was ultimately
+    // chosen given that it has proven to be the most optimal for high-contention scenarios,
+    // while still offering similar performance to the others when contention is low.
+    //
+
     public readonly partial struct SnowflakeId
     {
         private static long _lastTimestamp;
-        private static int _lastSequence;
-        private static int _incrementReservation;
+        private static ushort _lastSequence;
+        private static readonly object _idLock = new object();
 
         private const ushort MAX_SEQUENCE = ushort.MaxValue;
-        private const short MACHINE_ID = 12345;
+        private static readonly ushort MACHINE_ID = GetMachineId();
 
+        private static string MachineName
+        {
+            get
+            {
+                try
+                {
+                    return Dns.GetHostName();
+                }
+                catch
+                {
+                    try
+                    {
+                        return Environment.MachineName;
+                    }
+                    catch
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates a new <see cref="SnowflakeId"/>
+        /// </summary>
+        /// <returns>A new <see cref="SnowflakeId"/>.</returns>
         public static SnowflakeId NewId()
         {
             while (true)
             {
-                long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-
-                if (!TryStartSequence(timestamp, out int? sequenceNumber))
+                lock (_idLock)
                 {
-                    if (!TryIncrementSequence(timestamp, out sequenceNumber))
+                    long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+                    if (timestamp == _lastTimestamp)
                     {
-                        continue;
+                        if (_lastSequence == MAX_SEQUENCE)
+                        {
+                            continue;
+                        }
+
+                        _lastSequence++;
                     }
-                }
+                    else
+                    {
+                        _lastSequence = 0;
+                    }
 
-                return new SnowflakeId(timestamp, MACHINE_ID, (short)(sequenceNumber.Value & 0xFFFF));
+                    _lastTimestamp = timestamp;
+
+                    return new SnowflakeId(timestamp, MACHINE_ID, _lastSequence);
+                }
             }
         }
 
-        private static bool TryStartSequence(long timestamp, [NotNullWhen(true)] out int? sequenceStart)
+        /// <summary>
+        /// Returns a new <see cref="SnowflakeId"/> which only holds timestamp information.
+        /// </summary>
+        /// <returns>A new <see cref="SnowflakeId"/> where all bits are zeroed except the timestamp.</returns>
+        public static SnowflakeId NewTimestamp()
         {
-            // I think we require an Interlocked read here; we cant misread it and (incorrectly) assume that someone else
-            // already started when they havent actually
-            long lastTimestamp = Interlocked.Read(ref _lastTimestamp);
-
-            if (timestamp <= lastTimestamp)
-            {
-                sequenceStart = null;
-                return false;
-            }
-
-            if (Interlocked.CompareExchange(ref _lastTimestamp, timestamp, lastTimestamp) == lastTimestamp)
-            {
-                Interlocked.Exchange(ref _lastSequence, 0);
-                sequenceStart = 0;
-                return true;
-            }
-
-            sequenceStart = null;
-            return false;
+            return NewTimestamp(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
         }
 
-        private static bool TryIncrementSequence(long timestamp, [NotNullWhen(true)] out int? newSequence)
+        /// <summary>
+        /// Returns a new <see cref="SnowflakeId"/> which only holds timestamp information.
+        /// </summary>
+        /// <param name="timestamp">The timestamp for the <see cref="SnowflakeId"/>.</param>
+        /// <returns>A new <see cref="SnowflakeId"/> where all bits are zeroed except the <paramref name="timestamp"/>.</returns>
+        public static SnowflakeId NewTimestamp(long timestamp)
         {
-            while (true)
-            {
-                int oldSequence = Volatile.Read(ref _lastSequence);
-                if (oldSequence > MAX_SEQUENCE)
-                {
-                    SpinUntilNextMillis(timestamp);
-                    newSequence = null;
-                    return false;
-                }
-
-                if (!TryReserveIncrement(timestamp))
-                {
-                    newSequence = null;
-                    return false;
-                }
-
-                newSequence = oldSequence + 1;
-
-                // What we can do is instead of locking, is compare_xchg a bool (or rather an int)
-                // and if we get got it, try to compare_xchg the incremented one into _lastSequence (we still need to do
-                // this even though we've basically reserved a spot for ourselves because of the TryStartSequence, they
-                // also work with (i.e. xchg) _lastSequence); else, spinwait will checking
-                // if timestamp != DateTimeOffset.UtcNow.ToUnitTimeMilliseconds(), and ret false in that case
-
-                if (Interlocked.CompareExchange(ref _lastSequence, newSequence.Value, oldSequence) == oldSequence)
-                {
-                    Interlocked.Exchange(ref _incrementReservation, 0);
-                    break;
-                }
-                else
-                {
-                    Interlocked.Exchange(ref _incrementReservation, 0);
-                }
-            }
-
-            return true;
+            return new SnowflakeId(timestamp, 0, 0);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool TryReserveIncrement(long currentTimestamp)
+        private static ushort GetMachineId()
         {
-            SpinWait spinWait = default;
-            do
-            {
-                // Allow threads which are spinning to. Acts as sort of timeout
-                if (currentTimestamp != DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
-                {
-                    return false;
-                }
-
-                spinWait.SpinOnce();
-            } while (Interlocked.CompareExchange(ref _incrementReservation, 1, 0) == 0);
-
-            return true;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void SpinUntilNextMillis(long currentTimestamp)
-        {
-            SpinWait spinWait = default;
-            while (currentTimestamp != DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
-            {
-                spinWait.SpinOnce();
-            }
+            int hash = HashCode.Combine(MachineName.GetHashCode(), Environment.ProcessId);
+            return (ushort)(hash & 0xffff);
         }
     }
 }
