6189f2c85956180707b2a9a6355466fcbc9b5af3
Specyy <121744367+Specyy@users.noreply.github.com>
2025-01-14 06:26:13 -0800

    Implement request and response abstract packet serializers

---
+++ Sphynx/Network/Serialization/Packet/RequestPacketSerializer.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Sphynx.Core;
+using Sphynx.Network.PacketV2.Request;
+
+namespace Sphynx.Network.Serialization.Packet
+{
+    public abstract class RequestPacketSerializer<T> : PacketSerializer<T> where T : SphynxRequestPacket
+    {
+        protected sealed override unsafe int GetMaxPacketSize(T packet)
+        {
+            return SnowflakeId.SIZE + sizeof(Guid) + GetMaxPacketSizeInternal(packet);
+        }
+
+        protected abstract int GetMaxPacketSizeInternal(T packet);
+
+        protected sealed override void Serialize(T packet, ref BinarySerializer serializer)
+        {
+            serializer.WriteSnowflakeId(packet.UserId);
+            serializer.WriteGuid(packet.SessionId);
+
+            SerializeInternal(packet, ref serializer);
+        }
+
+        protected abstract void SerializeInternal(T packet, ref BinarySerializer serializer);
+
+        protected sealed override T Deserialize(ref BinaryDeserializer deserializer)
+        {
+            var userId = deserializer.ReadSnowflakeId();
+            var sessionId = deserializer.ReadGuid();
+            var requestInfo = new RequestPacketInfo { UserId = userId, SessionId = sessionId };
+
+            return DeserializeInternal(ref deserializer, requestInfo);
+        }
+
+        protected abstract T DeserializeInternal(ref BinaryDeserializer deserializer, RequestPacketInfo requestInfo);
+    }
+
+    public struct RequestPacketInfo
+    {
+        public SnowflakeId UserId { get; init; }
+        public Guid SessionId { get; init; }
+    }
+}
---
+++ Sphynx/Network/Serialization/Packet/ResponsePacketSerializer.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Sphynx.Core;
+using Sphynx.Network.PacketV2.Response;
+
+namespace Sphynx.Network.Serialization.Packet
+{
+    public abstract class ResponsePacketSerializer<T> : PacketSerializer<T> where T : SphynxResponsePacket
+    {
+        protected sealed override int GetMaxPacketSize(T packet)
+        {
+            return sizeof(SphynxErrorCode) + GetMaxPacketSizeInternal(packet);
+        }
+
+        protected abstract int GetMaxPacketSizeInternal(T packet);
+
+        protected sealed override void Serialize(T packet, ref BinarySerializer serializer)
+        {
+            serializer.WriteEnum(packet.ErrorCode);
+
+            SerializeInternal(packet, ref serializer);
+        }
+
+        protected abstract void SerializeInternal(T packet, ref BinarySerializer serializer);
+
+        protected sealed override T Deserialize(ref BinaryDeserializer deserializer)
+        {
+            var errorCode = deserializer.ReadEnum<SphynxErrorCode>();
+            var responseInfo = new ResponsePacketInfo { ErrorCode = errorCode };
+
+            return DeserializeInternal(ref deserializer, responseInfo);
+        }
+
+        protected abstract T DeserializeInternal(ref BinaryDeserializer deserializer, ResponsePacketInfo responseInfo);
+    }
+
+    public struct ResponsePacketInfo
+    {
+        public SphynxErrorCode ErrorCode { get; init; }
+    }
+}
