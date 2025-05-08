a063f0e658125958f3f7ddf620a0608b36183d9c
Specyy <121744367+Specyy@users.noreply.github.com>
2025-05-08 02:32:08 -0700

    Finish weak pool implementation

--- Sphynx/Storage/WeakObjectPool.cs
+++ Sphynx/Storage/WeakObjectPool.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Sphynx.Storage
 {
@@ -10,7 +11,7 @@ namespace Sphynx.Storage
     /// they can be taken from the pool.
     /// </summary>
     /// <typeparam name="T">The type of objects within this pool.</typeparam>
-    public class FixedObjectPool<T> where T : class
+    public class WeakObjectPool<T> where T : class
     {
         // The following implementation is based on Roslyn's ObjectPool`1 source code:
         // https://github.com/dotnet/roslyn/blob/main/src/Dependencies/PooledObjects/ObjectPool%601.cs
@@ -45,10 +46,9 @@ namespace Sphynx.Storage
         // Incremented each time an item is inserted into the pool. Only applicable when _fastChecks is false.
         private int _version;
 
-        // We store the first item in its own field since we expect to be able to satisfy most requests
-        // with it.
+        // We store the first item in its own field since we expect to be able to satisfy most requests with it.
         private T? _firstItem;
-        private readonly T?[] _items;
+        private readonly WeakReference<T>?[] _items;
 
         /// <summary>
         /// Creates a new fixed size pool
@@ -56,9 +56,9 @@ namespace Sphynx.Storage
         /// <param name="size">The number of items of <typeparamref name="T"/> which the pool can hold at once.</param>
         /// <param name="fastChecks">Whether to perform lockless optimizations which can falsely report pool emptiness,
         /// but can increase performance.</param>
-        public FixedObjectPool(int size, bool fastChecks = true)
+        public WeakObjectPool(int size, bool fastChecks = true)
         {
-            _items = new T[size - 1];
+            _items = new WeakReference<T>?[size - 1];
             _fastChecks = fastChecks;
         }
 
@@ -68,13 +68,7 @@ namespace Sphynx.Storage
             {
                 int version = _fastChecks ? -1 : Volatile.Read(ref _version);
 
-                // If allowed, we de not synchronize our initial read.
-                // In the worst case, we miss some recently returned objects.
-                item = _fastChecks ? _firstItem : Volatile.Read(ref _firstItem);
-
-                bool reservedFirstItem = item is not null && item == Interlocked.CompareExchange(ref _firstItem, null, item);
-
-                if (reservedFirstItem)
+                if (TryTakeFast(out item))
                     return true;
 
                 if (TryTakeSlow(out item))
@@ -88,18 +82,32 @@ namespace Sphynx.Storage
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool TryTakeFast(out T? item)
+        {
+            // If allowed, we de not synchronize our initial read.
+            // In the worst case, we miss some recently returned objects.
+            item = _fastChecks ? _firstItem : Volatile.Read(ref _firstItem);
+
+            bool reservedFirstItem = item is not null && item == Interlocked.CompareExchange(ref _firstItem, null, item);
+
+            return reservedFirstItem;
+        }
+
         private bool TryTakeSlow(out T? item)
         {
             for (int i = 0; i < _items.Length; i++)
             {
-                // If allowed, we de not synchronize our read.
-                // In the worst case, we miss some recently returned objects.
-                item = _fastChecks ? _items[i] : Volatile.Read(ref _items[i]);
+                // If allowed, we de not synchronize our read. In the worst case, we miss some recently returned objects.
+                var itemRef = _fastChecks ? _items[i] : Volatile.Read(ref _items[i]);
 
-                bool reservedItem = item is not null && item == Interlocked.CompareExchange(ref _items[i], null, item);
+                if (itemRef is null)
+                    continue;
 
-                if (reservedItem)
-                    return true;
+                if (itemRef != Interlocked.CompareExchange(ref _items[i], null, itemRef) || !itemRef.TryGetTarget(out item))
+                    continue;
+
+                return true;
             }
 
             item = null;
@@ -108,53 +116,64 @@ namespace Sphynx.Storage
 
         public bool Return(T obj)
         {
-            Debug.Assert(obj != null);
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
 
+            return ReturnFast(obj) || ReturnSlow(obj);
+        }
+
+        private bool ReturnFast(T obj)
+        {
             var firstItem = _fastChecks ? _firstItem : Volatile.Read(ref _firstItem);
 
-            if (firstItem is null)
-            {
-                if (_fastChecks)
-                {
-                    // In the worst case, two objects may be stored into same slot.
-                    // It is very unlikely to happen and will only mean that one of the objects will get collected.
-                    _firstItem = firstItem;
-                    return true;
-                }
+            // The first slot is already full.
+            if (firstItem is not null)
+                return false;
 
-                if (firstItem == Interlocked.CompareExchange(ref _firstItem, obj, firstItem))
-                {
-                    Interlocked.Increment(ref _version);
-                    return true;
-                }
+            // Try a non-interlocked write into the first slot. May or may not be replaced by other racing calls.
+            if (_fastChecks)
+            {
+                // In the worst case, two objects may be stored into same slot.
+                // It is very unlikely to happen and will only mean that one of the objects will get collected.
+                _firstItem = firstItem;
+                return true;
             }
 
-            return ReturnSlow(obj);
+            // Try and reserve the first slot for ourselves.
+            if (firstItem == Interlocked.CompareExchange(ref _firstItem, obj, firstItem))
+            {
+                Interlocked.Increment(ref _version);
+                return true;
+            }
+
+            return false;
         }
 
         private bool ReturnSlow(T obj)
         {
-            Debug.Assert(obj != null);
+            WeakReference<T>? objReference = null;
 
             for (int i = 0; i < _items.Length; i++)
             {
-                var item = _fastChecks ? _items[i] : Volatile.Read(ref _items[i]);
+                var itemRef = _fastChecks ? _items[i] : Volatile.Read(ref _items[i]);
 
-                if (item is null)
+                if (itemRef is not null)
+                    continue;
+
+                if (_fastChecks)
                 {
-                    if (_fastChecks)
-                    {
-                        // In the worst case, two objects may be stored into same slot.
-                        // It is very unlikely to happen and will only mean that one of the objects will get collected.
-                        _items[i] = obj;
-                        return true;
-                    }
+                    objReference = new WeakReference<T>(obj);
 
-                    if (item == Interlocked.CompareExchange(ref _items[i], obj, item))
-                    {
-                        Interlocked.Increment(ref _version);
-                        return true;
-                    }
+                    // In the worst case, two objects may be stored into same slot.
+                    // It is very unlikely to happen and will only mean that one of the objects will get collected.
+                    _items[i] = objReference;
+                    return true;
+                }
+
+                if (null == Interlocked.CompareExchange(ref _items[i], objReference ??= new WeakReference<T>(obj), null))
+                {
+                    Interlocked.Increment(ref _version);
+                    return true;
                 }
             }
 
