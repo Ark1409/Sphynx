195918b53bf091c9bf4dd4f145124673c61d689b
Specyy <121744367+Specyy@users.noreply.github.com>
2025-05-22 00:52:00 -0700

    Add self and message info persistence models

---
+++ Sphynx.ServerV2/Persistence/User/LastReadMessageInfo.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections;
+using MongoDB.Bson.Serialization.Attributes;
+using Sphynx.Core;
+using Sphynx.ModelV2.User;
+
+namespace Sphynx.ServerV2.Persistence.User
+{
+    [BsonIgnoreExtraElements]
+    public class LastReadMessageInfo : ILastReadMessageInfo
+    {
+        private readonly IDictionary<SnowflakeId, SnowflakeId> _lastReadMessages;
+
+        public LastReadMessageInfo() : this(new Dictionary<SnowflakeId, SnowflakeId>())
+        {
+        }
+
+        public LastReadMessageInfo(IDictionary<SnowflakeId, SnowflakeId> lastReadMessages)
+        {
+            _lastReadMessages = lastReadMessages;
+        }
+
+        public IEnumerator<KeyValuePair<SnowflakeId, SnowflakeId>> GetEnumerator() =>
+            _lastReadMessages.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _lastReadMessages.GetEnumerator();
+
+        public void Add(KeyValuePair<SnowflakeId, SnowflakeId> item) => _lastReadMessages.Add(item);
+
+        public void Clear() => _lastReadMessages.Clear();
+
+        public bool Contains(KeyValuePair<SnowflakeId, SnowflakeId> item) => _lastReadMessages.Contains(item);
+
+        public void CopyTo(KeyValuePair<SnowflakeId, SnowflakeId>[] array, int arrayIndex) =>
+            _lastReadMessages.CopyTo(array, arrayIndex);
+
+        public bool Remove(KeyValuePair<SnowflakeId, SnowflakeId> item) => _lastReadMessages.Remove(item);
+
+        public int Count => _lastReadMessages.Count;
+
+        public bool IsReadOnly => _lastReadMessages.IsReadOnly;
+
+        public void Add(SnowflakeId key, SnowflakeId value) => _lastReadMessages.Add(key, value);
+
+        public bool ContainsKey(SnowflakeId key) => _lastReadMessages.ContainsKey(key);
+
+        public bool Remove(SnowflakeId key) => _lastReadMessages.Remove(key);
+
+        public bool TryGetValue(SnowflakeId key, out SnowflakeId value) =>
+            _lastReadMessages.TryGetValue(key, out value);
+
+        public SnowflakeId this[SnowflakeId key]
+        {
+            get => _lastReadMessages[key];
+            set => _lastReadMessages[key] = value;
+        }
+
+        public ICollection<SnowflakeId> Keys => _lastReadMessages.Keys;
+
+        public ICollection<SnowflakeId> Values => _lastReadMessages.Values;
+    }
+}
---
+++ Sphynx.ServerV2/Persistence/User/SphynxSelfInfo.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Immutable;
+using MongoDB.Bson.Serialization.Attributes;
+using Sphynx.Core;
+using Sphynx.ModelV2.User;
+
+namespace Sphynx.ServerV2.Persistence.User
+{
+    /// <summary>
+    /// Represents a complete representation of a <c>Sphynx</c> user within the database.
+    /// </summary>
+    /// <seealso cref="ISphynxSelfInfo"/>
+    [BsonIgnoreExtraElements]
+    public class SphynxSelfInfo : SphynxUserInfo, ISphynxSelfInfo
+    {
+        public const string FRIENDS_FIELD = "friends";
+        public const string ROOMS_FIELD = "rooms";
+        public const string LAST_READ_MSGS_FIELD = "last_read";
+        public const string OUT_FRIEND_REQS_FIELD = "out_reqs";
+        public const string INC_FRIEND_REQS_FIELD = "inc_recs";
+        public const string PWD_FIELD = "pwd";
+        public const string PWD_SALT_FIELD = "pwd_salt";
+
+        [BsonElement(FRIENDS_FIELD)]
+        public ISet<SnowflakeId> Friends { get; set; } = ImmutableHashSet<SnowflakeId>.Empty;
+
+        [BsonElement(ROOMS_FIELD)]
+        public ISet<SnowflakeId> Rooms { get; set; } = ImmutableHashSet<SnowflakeId>.Empty;
+
+        [BsonElement(LAST_READ_MSGS_FIELD)]
+        public ILastReadMessageInfo LastReadMessages { get; set; }
+
+        [BsonElement(OUT_FRIEND_REQS_FIELD)]
+        public ISet<SnowflakeId> OutgoingFriendRequests { get; set; } = ImmutableHashSet<SnowflakeId>.Empty;
+
+        [BsonElement(INC_FRIEND_REQS_FIELD)]
+        public ISet<SnowflakeId> IncomingFriendRequests { get; set; } = ImmutableHashSet<SnowflakeId>.Empty;
+
+        /// <summary>
+        /// The hashed password for this Sphynx user, as a base-64 string.
+        /// </summary>
+        [BsonElement(PWD_FIELD)]
+        internal string? Password { get; set; }
+
+        /// <summary>
+        /// The salt for the password of this Sphynx user.
+        /// </summary>
+        [BsonElement(PWD_SALT_FIELD)]
+        internal string? PasswordSalt { get; set; }
+
+        public SphynxSelfInfo(SnowflakeId userId, string userName, SphynxUserStatus userStatus) : base(userId, userName, userStatus)
+        {
+        }
+
+        public SphynxSelfInfo(ISphynxUserInfo userInfo) : base(userInfo.UserId, userInfo.UserName, userInfo.UserStatus)
+        {
+        }
+
+        public SphynxSelfInfo(ISphynxSelfInfo selfInfo) : base(selfInfo.UserId, selfInfo.UserName, selfInfo.UserStatus)
+        {
+            Friends = selfInfo.Friends;
+            Rooms = selfInfo.Rooms;
+            LastReadMessages = selfInfo.LastReadMessages;
+            OutgoingFriendRequests = selfInfo.OutgoingFriendRequests;
+            IncomingFriendRequests = selfInfo.IncomingFriendRequests;
+
+            if (selfInfo is SphynxSelfInfo dbSelfInfo)
+            {
+                Password = dbSelfInfo.Password;
+                PasswordSalt = dbSelfInfo.PasswordSalt;
+            }
+        }
+
+        public SphynxSelfInfo(SnowflakeId userId,
+            string userName,
+            SphynxUserStatus userStatus,
+            string? password,
+            string? passwordSalt,
+            ISet<SnowflakeId> friends,
+            ISet<SnowflakeId> rooms,
+            ILastReadMessageInfo lastReadMessages,
+            ISet<SnowflakeId> outgoingFriendRequests,
+            ISet<SnowflakeId> incomingFriendRequests)
+            : base(userId, userName, userStatus)
+        {
+            Friends = friends;
+            Rooms = rooms;
+            LastReadMessages = lastReadMessages;
+            OutgoingFriendRequests = outgoingFriendRequests;
+            IncomingFriendRequests = incomingFriendRequests;
+            Password = password;
+            PasswordSalt = passwordSalt;
+        }
+
+        private void Initialize(ISphynxUserInfo userInfo)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => UserId.GetHashCode();
+    }
+}
