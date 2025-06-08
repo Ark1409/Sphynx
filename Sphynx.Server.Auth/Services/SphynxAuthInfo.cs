e6c969179d7099944924db8f8da366f9221f01d9
Specyy <121744367+Specyy@users.noreply.github.com>
2025-06-07 20:10:06 -0700

    Move models to `Model`

--- Sphynx.Server.Auth/Services/SphynxAuthInfo.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using Sphynx.Server.Auth.Model;
-
-namespace Sphynx.Server.Auth.Services
-{
-    public record struct SphynxAuthInfo(SphynxAuthUser User, Guid SessionId);
-}
