99569b61a3d54ba068ac095ba92718060aded956
Specyy <121744367+Specyy@users.noreply.github.com>
2025-07-03 03:24:16 -0700

    Create server test project

---
+++ Sphynx.Server.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sphynx.Server.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
