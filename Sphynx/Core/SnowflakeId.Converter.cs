334769c00c60af9ac790f3c5572cacfa08e04da2
Specyy <121744367+Specyy@users.noreply.github.com>
2025-08-13 12:34:37 -0700

    Truncate `SnowflakeId` to 80 bits and store it in "big-endian" by default so that it is easy to serialize

--- Sphynx/Core/SnowflakeId.Converter.cs
+++ Sphynx/Core/SnowflakeId.Converter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Ark -α- & Specyy. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
 using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -11,7 +12,6 @@ namespace Sphynx.Core
     {
         public override SnowflakeId Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            // TODO: Copy string
             string? idString = reader.GetString();
 
             if (!SnowflakeId.TryParse(idString, out var id))
@@ -22,8 +22,13 @@ namespace Sphynx.Core
 
         public override void Write(Utf8JsonWriter writer, SnowflakeId value, JsonSerializerOptions options)
         {
-            // TODO: Copy string
-            writer.WriteStringValue(value.ToString());
+            Span<char> valueString = stackalloc char[20];
+
+            bool formatted = value.TryFormat(valueString, out int charsWritten);
+            Debug.Assert(formatted);
+            Debug.Assert(charsWritten == valueString.Length);
+
+            writer.WriteStringValue(valueString);
         }
     }
 }
