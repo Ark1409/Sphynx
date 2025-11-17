acef1ebf56e631bb4b8864eb6ab506ee27b5b3d2
Specyy <121744367+Specyy@users.noreply.github.com>
2025-11-17 11:00:50 -0800

    Create chat server

---
+++ Sphynx.Server.Chat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sphynx.Server.Chat
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
