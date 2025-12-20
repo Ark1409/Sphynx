ccbec9344c8bb0aeea3a1332e3ff89b4d16c2f82
Specyy <121744367+Specyy@users.noreply.github.com>
2025-12-19 23:50:27 -0800

    Create friend request DTO

---
+++ Sphynx/Network/Packet/Request/AddFriendRequest.cs
@@ -0,0 +1,46 @@
+﻿using Sphynx.Core;
+using Sphynx.Network.Packet.Response;
+using Sphynx.Utils;
+
+namespace Sphynx.Network.Packet.Request
+{
+    /// <inheritdoc cref="SphynxPacketType.ADD_FRIEND_REQ"/>
+    public sealed class AddFriendRequest : SphynxRequest<AddFriendResponse>, IEquatable<AddFriendRequest>
+    {
+        /// <inheritdoc/>
+        public override SphynxPacketType PacketType => SphynxPacketType.ADD_FRIEND_REQ;
+
+        /// <summary>
+        /// The user IDs of the users for which to retrieve information.
+        /// </summary>
+        public Guid OtherId { get; set; }
+
+        public AddFriendRequest()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FetchUsersRequest"/>.
+        /// </summary>
+        public AddFriendRequest(Guid sessionId) : base(sessionId)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FetchUsersRequest"/>.
+        /// </summary>
+        /// <param name="sessionId">The JWT access token for this request.</param>
+        public AddFriendRequest(Guid sessionId, Guid otherId) : base(sessionId)
+        {
+            OtherId = otherId;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(AddFriendRequest? other) => base.Equals(other) && OtherId == other?.OtherId;
+
+        public override AddFriendResponse CreateResponse(SphynxErrorInfo errorInfo) => new AddFriendResponse(errorInfo)
+        {
+            RequestTag = RequestTag
+        };
+    }
+}
---
+++ Sphynx/Network/Packet/Response/AddFriendResponse.cs
@@ -0,0 +1,38 @@
+﻿using Sphynx.Core;
+using Sphynx.Model.User;
+
+namespace Sphynx.Network.Packet.Response
+{
+    /// <inheritdoc cref="SphynxPacketType.ADD_FRIEND_RES"/>
+    public sealed class AddFriendResponse : SphynxResponse, IEquatable<AddFriendResponse>
+    {
+        /// <inheritdoc/>
+        public override SphynxPacketType PacketType => SphynxPacketType.ADD_FRIEND_RES;
+
+        public SphynxFriendRequest? FriendRequest { get; set; }
+
+        public AddFriendResponse(SphynxFriendRequest friendRequest)
+        {
+            FriendRequest = friendRequest;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AddFriendResponse"/>.
+        /// </summary>
+        /// <param name="errorInfo">Error code for kick attempt.</param>
+        public AddFriendResponse(SphynxErrorCode errorInfo = SphynxErrorCode.SUCCESS) : base(errorInfo)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AddFriendResponse"/>.
+        /// </summary>
+        /// <param name="errorInfo">Error code for kick attempt.</param>
+        public AddFriendResponse(SphynxErrorInfo errorInfo) : base(errorInfo)
+        {
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(AddFriendResponse? other) => base.Equals(other);
+    }
+}
