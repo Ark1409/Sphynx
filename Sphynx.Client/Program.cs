095fc7a3343cd97af3c0c52acdc55baef665d855
Ark1409 <ark1409dev@gmail.com>
2025-12-28 17:44:35 -0800

    Added dotnet-curses submodule project

--- Sphynx.Client/Program.cs
+++ Sphynx.Client/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Sphynx.Client
+﻿using Mindmagma.Curses;
+
+namespace Sphynx.Client
 {
     internal static class Program
     {
         static int Main(string[] args)
         {
+            var scr = NCurses.InitScreen();
             Console.WriteLine("Hello World");
+            NCurses.WindowAddString(scr, "Hello World");
+            NCurses.Refresh();
+            NCurses.Raw();
+            NCurses.GetChar();
+            NCurses.EndWin();
             return 0;
         }
     }
